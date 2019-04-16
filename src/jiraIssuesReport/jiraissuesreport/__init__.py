b65305271dceab71d8b238184836cafffaad10a7
marco cipri <marcocipri@gmail.com>
2019-04-16 15:48:51 +0200

    project and main module creation

---
+++ src/jiraIssuesReport/jiraissuesreport/__init__.py
@@ -0,0 +1,22 @@
+'''
+jiraIssuesReport: Main module
+
+Copyright 2019, marco cipri
+Licensed under MIT.
+'''
+
+
+def main():
+    '''
+    Main function of the boilerplate code is the entry point of the 'jiraissuesreport' executable script (defined in setup.py).
+    
+    Use doctests, those are very helpful.
+    
+    >>> main()
+    Hello
+    >>> 2 + 2
+    4
+    '''
+    
+    print("Hello")
+
